605e817c857cd4271bff80e2080724b12a3ec594
Altynbek <aton.zhumabergen@mail.ru>
2025-01-14 18:16:24 +0500

    add CQRS+Mediatr,Carter

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,26 @@
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
+using MediatR;
+
+namespace Catalog.API.Products.CreateProduct
+{
+    public record CreateProductCommand(string Name,List<string> Category,string Description,string ImagePath,decimal Price)
+        :ICommand<CreateProductResult>;
+    public record CreateProductResult(Guid Id);
+    internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+    {
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            var product = new Product
+            {
+                Name = command.Name,
+                Category = command.Category,
+                Description = command.Description,
+                ImagePath = command.ImagePath,
+                Price = command.Price,
+            };
+            return new CreateProductResult(Guid.NewGuid());
+
+        }
+    }
+}
